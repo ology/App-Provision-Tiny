de1d108fd366c0b6a069e96f5b4d7be396949337
Gene Boggs <gene.boggs@gmail.com>
2013-11-21 14:00:44 -0800

    Fixed indentation

--- bar-chart.js
+++ bar-chart.js
@@ -50,43 +50,43 @@ bar.append("text")
 
 // tsv-chart:
 
-// Set our range:
+// Reset the range:
 x = d3.scale.linear()
     .range([0, width]);
 
 // Reset the text offset:
 text_offset = 13;
 
-// Get a new chart:
+// Reset the chart:
 chart = d3.select(".tsv-chart")
     .attr("width", width);
 
 // Import the data:
 d3.tsv("bar-chart.dat", type, function(error, data) {
 
-  // Set the domain inside a callback:
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+    // Set the domain inside a callback:
+    x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-  // Set the height dimension:
-  chart.attr("height", barHeight * data.length);
+    // Set the height dimension:
+    chart.attr("height", barHeight * data.length);
 
-  // Add translation to the SVG g transform:
-  bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    // Add translation to the SVG g transform:
+    bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-  // Add a bar:
-  bar.append("rect")
-      .attr("width", function(d) { return x(d.value); })
-      .attr("height", barHeight - 1);
+    // Add a bar:
+    bar.append("rect")
+        .attr("width", function(d) { return x(d.value); })
+        .attr("height", barHeight - 1);
 
-  // Add the bar label:
-  bar.append("text")
-      .attr("x", function(d) { return x(d.value) - text_offset; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.value; });
+    // Add the bar label:
+    bar.append("text")
+        .attr("x", function(d) { return x(d.value) - text_offset; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.value; });
 });
 
 // Set the domain inside this callback.

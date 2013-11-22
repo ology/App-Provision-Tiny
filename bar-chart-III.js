15e1803cd9b805f50e699d935c411f5f95e3a41a
Gene Boggs <gene.boggs@gmail.com>
2013-11-22 12:04:41 -0800

    Re-added handy comments

--- bar-chart-III.js
+++ bar-chart-III.js
@@ -1,38 +1,47 @@
+// Self study from http://bost.ocks.org/mike/bar/3/
+// Horizontal bar chart
 
+// Set our dimensions
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
+    width  = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+// Set the range bands
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
 
+// Set the range
 var y = d3.scale.linear()
     .range([height, 0]);
 
+// Get axes
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
-
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
     .ticks(10, "%");
 
+// Set the chart object
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Load external data
 d3.tsv("bar-chart-III.dat", type, function(error, data) {
+
+  // Set the domains, now that we have data
   x.domain(data.map(function(d) { return d.letter; }));
   y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
+  // Add the axis to the chart
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
-
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
@@ -43,6 +52,7 @@ d3.tsv("bar-chart-III.dat", type, function(error, data) {
       .style("text-anchor", "end")
       .text("Frequency");
 
+  // Set the bar object
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
@@ -54,6 +64,7 @@ d3.tsv("bar-chart-III.dat", type, function(error, data) {
 
 });
 
+// Set the domain inside this callback.
 function type(d) {
   d.frequency = +d.frequency;
   return d;

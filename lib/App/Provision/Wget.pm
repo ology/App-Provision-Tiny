2a0e9d309f15efe56a06c642b2949c3dd206611c
Gene Boggs <gene.boggs@online-rewards.com>
2014-03-14 14:38:31 -0400

    Handy

---
+++ lib/App/Provision/Wget.pm
@@ -0,0 +1,23 @@
+package App::Provision::Wget;
+use strict;
+use warnings;
+use parent qw( App::Provision::Tiny );
+
+sub meet
+{
+    my $self = shift;
+    if ($self->{system} eq 'osx' )
+    {
+        $self->recipe(
+          [qw( brew install wget )],
+        );
+    }
+    elsif ($self->{system} eq 'apt' )
+    {
+        $self->recipe(
+          [qw( sudo apt-get install wget )],
+        );
+    }
+}
+
+1;
